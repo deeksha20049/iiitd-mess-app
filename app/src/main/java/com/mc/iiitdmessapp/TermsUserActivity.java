f2665b557132ec5ca8ea9b452eed5d78a18a8ec3
sc0rp10n-w11lappy <pragyan52yadav@gmail.com>
2023-03-28 10:15:26 +0530

    welcome, terms backup for safety

--- app/src/main/java/com/mc/iiitdmessapp/TermsUserActivity.java
+++ app/src/main/java/com/mc/iiitdmessapp/TermsUserActivity.java
@@ -1,15 +1,27 @@
 package com.mc.iiitdmessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TermsUserActivity extends AppCompatActivity {
+    Button acpt;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_terms_user);
+        acpt = (Button) findViewById(R.id.termsUserBtn);
+        acpt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TermsUserActivity.this, DashboardUserActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
