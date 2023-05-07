90745007d56a7623e61d4c857ec2df1e083a51ee
Rohan <rohan19094@iiitd.ac.in>
2023-05-07 16:24:16 +0530

    Commiting

--- app/src/main/java/com/mc/iiitdmessapp/MenuUserFragment.java
+++ app/src/main/java/com/mc/iiitdmessapp/MenuUserFragment.java
@@ -4,29 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.Arrays;
 
 public class MenuUserFragment extends Fragment {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_menu_user, container, false);
-        Toast.makeText(view.getContext(), "User Menu", Toast.LENGTH_SHORT).show();
+
+        Spinner daySpinner = view.findViewById(R.id.day_spinner);
+        ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter.createFromResource(view.getContext(), R.array.days_of_week, android.R.layout.simple_spinner_item);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+
+        Spinner breakfastSpinner = view.findViewById(R.id.breakfast_spinner);
+        ArrayAdapter<CharSequence> mealAdapter = ArrayAdapter.createFromResource(view.getContext(), R.array.meal_types, android.R.layout.simple_spinner_item);
+        mealAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        breakfastSpinner.setAdapter(mealAdapter);
+
+        Spinner lunchSpinner = view.findViewById(R.id.lunch_spinner);
+        lunchSpinner.setAdapter(mealAdapter);
+
+        Spinner dinnerSpinner = view.findViewById(R.id.dinner_spinner);
+        dinnerSpinner.setAdapter(mealAdapter);
+
+        // Set default selection for meal spinners
+        int idliIndex = mealAdapter.getPosition("Idli");
+        breakfastSpinner.setSelection(idliIndex);
+        lunchSpinner.setSelection(idliIndex);
+        dinnerSpinner.setSelection(idliIndex);
+
+
+
         return view;
     }
 }
